66e4816f0dd830139b5cede4784c81817b758364
Mabi Sousa <maria_gs_cruz@estudante.sc.senai.br>
2021-08-20 20:26:40 -0300

    Criação da página de login

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,79 @@
+import React, {useCallback, useContext, useRef} from 'react';
+
+import { Main, Inputs,  FormButton, ImgLogo } from './style';
+import logo from '../../imgs/logo.svg';
+
+import { FormHandles } from '@unform/core'
+import { Form } from '@unform/web'
+import * as Yup from 'yup'
+
+import { AuthContext } from '../../hooks/Auth';
+import getValidationErrors from '../../utils/getValidationErrors';
+import Input from '../../components/Input';
+
+import { useHistory } from 'react-router-dom'
+
+interface SignInData {
+    email: string,
+    senha: string,
+}
+
+const Login: React.FC = () => {
+  
+    const formRef = useRef<FormHandles>(null);
+
+    const { signIn } = useContext(AuthContext);
+    const history = useHistory();
+
+    const handleSubmit = useCallback(async (data: SignInData) => {
+        console.log(data)
+        
+        try {
+            formRef.current?.setErrors({});
+
+            const schema = Yup.object().shape({
+                email: Yup.string()
+                .required("E-mail obrigatório")
+                .email("Insira um e-mail válido"),
+                senha: Yup.string()
+                .required("Senha obrigatória"),
+            });
+
+            await schema.validate(data, {
+                abortEarly: false,
+            });
+
+            await signIn({
+                email: data.email,
+                senha: data.senha
+            });
+            history.push('home')
+        } catch (e) {
+            
+            if(e instanceof Yup.ValidationError) {
+
+                const errors = getValidationErrors(e);
+                formRef.current?.setErrors(errors);
+
+                return
+            }
+        }
+    }, [ signIn, history ]);
+    return (
+    <>
+        <Main>
+            <Inputs>
+                <Form ref={ formRef } onSubmit={ handleSubmit }>
+                        <ImgLogo src={logo} alt="WEG" />
+
+                        <Input name="email" type="text">Usuário</Input>
+                        <Input name="senha" type="password">Senha</Input>
+
+                        <FormButton type="submit" value="Entrar"/>
+                </Form>
+            </Inputs>
+        </Main>
+    </>
+)};
+
+export default Login;
---
+++ src/pages/Login/style.ts
@@ -0,0 +1,98 @@
+import styled from 'styled-components';
+
+export const Main = styled.main`
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 5%;
+`;
+
+export const ImgLogo = styled.img`
+  width: 150px;
+  background-color: white;
+  margin-top: -50px;
+  margin-bottom: 100px;`;
+
+export const Inputs = styled.div`
+  width: 450px;
+  height: 500px;
+  margin: auto;
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #DADCE0;
+  border-radius: .5rem;
+
+    form {
+      margin: 0 auto;
+      width: 360px;
+      border-radius: 1rem;
+      text-align: center;
+    }
+`;
+
+
+export const FormDiv = styled.div`
+  position: relative;
+  height: 48px;
+  margin-bottom: 1.5rem; 
+`;
+
+export const FormInput = styled.input`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  font-size: 1rem;
+  border: 1px solid #DADCE0;
+  border-radius: .5rem;
+  outline: none;
+  padding: 1rem;
+  background: none;
+  z-index: 1;
+
+  &:focus{
+    border: 1.5px solid rgb(0, 87, 157);
+  }
+
+  #form__input&:focus + #form__label{
+    top: -.5rem;
+    left: .8rem;
+    color: rgb(0, 87, 157);
+    font-size: .75rem;
+    font-weight: 500;
+    z-index: 10;
+}
+`;
+
+export const FormLabel = styled.label`
+  position: absolute;
+  left: 1rem;
+  top: 1rem;
+  padding: 0 .25rem;
+  background-color: #fff;
+  color: #80868B;
+  font-size: 1rem;
+  transition: .3s;
+`;
+
+export const FormButton = styled.input`
+  display: block;
+  padding: .75rem 2rem;
+  outline: none;
+  border: none;
+  background-color: rgb(0, 87, 157);
+  color: #fff;
+  font-size: 1rem;
+  border-radius: .5rem;
+  cursor: pointer;
+  transition: .3s;
+  margin: auto;
+
+    &:hover {
+      box-shadow: 0 10px 36px rgba(0,0,0,.15);
+    }
+
+  
+`;
