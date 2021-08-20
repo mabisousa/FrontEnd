f0d13eccd81b11957f8d96508379d0ed2af5a961
Mabi Sousa <maria_gs_cruz@estudante.sc.senai.br>
2021-08-20 20:48:19 -0300

    Componentização da header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Container } from './style'
+import logo from "../../imgs/logo-weg.svg"
+import bandeiraBrasil from "../../imgs/bandeiraBrasil.svg"
+import { Link } from "react-router-dom";
+
+const Header: React.FC = ({children,...props}) => (
+    <Container {...props}> 
+        <div id="aprovacao">
+            <Link to="/projetos"><img src={logo} /></Link>
+            {children}
+        </div>   
+        <div id="flagExit">
+            <img src={bandeiraBrasil} /> 
+            <Link to="/"><p>Sair</p></Link>
+        </div>
+    </Container>
+)
+
+export default Header;
---
+++ src/components/Header/style.ts
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 100%;
+    height: 12vh;
+    border-bottom: 2px solid #90C0E3;  
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    
+    #aprovacao{
+        width:220px;
+        display: flex;
+        align-items: center;
+
+        img{
+            margin:0 30px;
+            height: 70px;
+            cursor: pointer;
+        }
+        p {
+            font-weight: bold;
+            font-size: 16px;
+            cursor: pointer;
+            color: #00579D;
+        }
+    }
+
+    #flagExit{
+        width: 80px;
+        height: 30px;
+        margin: 5px;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+
+        img{
+            height: 30px;
+            width: 30px;
+            cursor: pointer;
+        }
+        
+        p {
+                font-size: 14px;
+                width: 40px;
+                color: #00579D;
+                cursor: pointer;
+                border-left: 1.5px solid #00579D;
+                text-align: center;
+        }
+    }
+`;
