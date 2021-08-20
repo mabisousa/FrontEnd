0b6c8fca69366e6faf53a2c8b871bf7d1b55e662
Mabi Sousa <maria_gs_cruz@estudante.sc.senai.br>
2021-08-20 20:41:35 -0300

    Criação de função para exibição de mensagem de erro

---
+++ src/utils/getValidationErrors.ts
@@ -0,0 +1,17 @@
+import { ValidationError } from 'yup'
+
+interface Errors {
+    [key: string]: string,
+}
+
+export default function getValidationErrors(e: ValidationError) {
+    const ValidationErrors: Errors = {};
+
+    e.inner.forEach(error => {
+        if(error.path) {
+            ValidationErrors[error.path] = error.message;
+        }
+    });
+
+    return ValidationErrors;
+}
