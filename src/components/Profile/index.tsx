7d23d7241575ce51a45e84af36043bff81f3e8af
Mabi Sousa <maria_gs_cruz@estudante.sc.senai.br>
2021-08-20 20:50:40 -0300

    Componentização da barra de perfil e notificação

---
+++ src/components/Profile/index.tsx
@@ -0,0 +1,34 @@
+import React, {HTMLAttributes} from "react";
+import { Container } from './style'
+import Notification from '../Notification'
+import avatar from "../../imgs/Avatar.svg"
+
+type ProfileProps  = HTMLAttributes<HTMLDivElement>;
+
+const Profile: React.FC<ProfileProps> = (props) => (
+    <Container {...props}>
+            <div>
+                    <div id="perfil">
+                        <img src={ avatar } alt="" />
+                        <div id="txt">
+                            <h2>Nome</h2>
+                            <p>Atuação</p>
+                        </div>
+                    </div>
+                    <div id="notifications">
+                        <h3>Notificações</h3>
+                        <div id="hold">
+                            <Notification></Notification>
+                            <Notification></Notification>
+                            <Notification></Notification>
+                            <Notification></Notification>
+                            <Notification></Notification>
+                            <Notification></Notification>
+                            <Notification></Notification>
+                        </div>
+                    </div>
+                </div>
+    </Container>
+)
+
+export default Profile;
---
+++ src/components/Profile/style.ts
@@ -0,0 +1,143 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 250px;
+    height: 550px;
+    background: #fff;
+
+    position: fixed;
+    top: 15%;
+    left: -180px;
+
+    padding: 10px;
+    border: 1px solid #00579D;
+    border-radius:00px 20px 20px 0px;
+    box-shadow: 0px 0px 5px 2px #e2e2e280;
+
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+
+    transition: 1s;
+    z-index: 10;
+
+    div {
+        width: 50px;
+        height: 99%;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        
+        transition: 1s;
+
+        &::after {
+            content: "";
+            position: absolute;
+            top: 45%;
+            right: -15px;   
+            border-top: 15px solid transparent;
+            border-bottom: 15px solid transparent;
+            border-left: 15px solid #014C88;
+        }
+        &::before {
+            content: "";
+            position: absolute;
+            top: 45%;
+            right: -13px;  
+            border-top: 15px solid transparent;
+            border-bottom: 15px solid transparent;
+            border-left: 15px solid #fff;
+            z-index: 2; 
+
+        }
+        #perfil {
+                display: flex;
+                flex-direction: column;
+                justify-content: start;
+                align-items: center;
+
+            #txt {
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
+                
+                width: 0;
+                height: 30px;
+                overflow: hidden;
+                transition: 1s;
+
+                h2 {
+                    font-size: 14px;
+                }
+                p {
+                    font-size: 12px;
+                }
+            }
+            img {
+                height: 50px;
+                width: 50px;
+                transition: 1s;
+            }
+        }
+
+        #notifications {
+            max-height: 70%;
+            min-height: 60%;
+            width: 0;
+
+            justify-content: start;
+
+            overflow: hidden;
+
+            h3 { 
+                border-bottom: 2px solid #28B9DA;
+                margin-bottom: 10px;
+                
+            }
+
+            #hold {
+                width: 100%;
+                border-left: 1px solid #e2e2e2;
+                padding:0 5%;
+                overflow-y: scroll;
+
+
+                &::-webkit-scrollbar {
+                    display: none;
+                }
+            }
+        }
+    }
+    &:hover {
+        left: 0;
+        
+        div {
+            width: 230px;
+
+            #perfil {
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: start;
+
+                img {
+                height: 130px;
+                width: 130px;
+                margin: 5% 17%;
+                transition: 1s;
+                }
+                #txt {
+                    width: 100%;
+                    height: auto;
+                    transition: 2s;
+                }
+            }
+            #notifications {
+                width: 100%;
+                padding: 15px;
+                transition: 1s;
+            }
+        }
+    }
+`;
