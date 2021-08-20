a37334041f80f5bc7fe3931414e65a0b7803c8b6
Mabi Sousa <maria_gs_cruz@estudante.sc.senai.br>
2021-08-20 20:54:53 -0300

    Criação de rotas

---
+++ src/routes/Route.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import {RouteProps as ReactRouteProps, Route as ReactRoute, Redirect} from 'react-router-dom'
+import { useAuth } from "../hooks/Auth";
+
+interface RouteProps extends ReactRouteProps {
+    isPrivate?: boolean,
+    component: React.ComponentType,
+}
+
+const Route: React.FC<RouteProps> = 
+    ({
+    isPrivate = false,
+    component: Component,
+    ...rest
+    }) => {
+        
+        const { usuario } = useAuth();
+    
+    return (
+        <ReactRoute 
+            {...rest}
+            render={({location}) => {
+                return isPrivate === !!usuario ?
+                ( 
+                    <Component/>
+                ):
+                (
+                    <Redirect to={{
+                        pathname: isPrivate ? "/" : "Home",
+                        state: { from: location},
+                    }}/>
+                )
+            }}
+        />
+    );
+}
+
+export default Route;
---
+++ src/routes/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Login from '../pages/Login';
+import Aprovacao from '../pages/Aprovacao';
+import Consultores from '../pages/Consultores';
+import Projeto from '../pages/Home/index';
+import Projeto2 from '../pages/display';
+import popUp from '../pages/projectPopUp/index';
+import popUpConsultores from '../pages/ConsultoresPopUp';
+
+const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" exact component={Login}/>
+    <Route path="/aprovacao" exact component={Aprovacao}/>
+    <Route path="/consultores" component={Consultores}/>
+    <Route path="/projetos" exact component={Projeto}/>
+    <Route path="/display2" exact component={Projeto2}/>
+    <Route path="/projetos/popUp" exact component={popUp}/>
+    <Route path="/popUp" exact component={popUpConsultores} />
+  </Switch>
+  );
+
+export default Routes;
