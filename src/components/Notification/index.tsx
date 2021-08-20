ac5b948ca9bbf89f5e72dc584a184dcba453e7fb
Mabi Sousa <maria_gs_cruz@estudante.sc.senai.br>
2021-08-20 20:49:09 -0300

    Componentização da barra de notificação

---
+++ src/components/Notification/index.tsx
@@ -0,0 +1,12 @@
+import React, { HTMLAttributes} from "react";
+import { Container } from './style'
+
+type NotificationProps = HTMLAttributes<HTMLDivElement>;
+
+const Notification: React.FC<NotificationProps> = (props) => (
+    <Container {...props}>
+
+    </Container>
+)
+
+export default Notification;
---
+++ src/components/Notification/style.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    min-height: 50px;
+    max-width: 100%;
+    margin:5px 0px;
+    border: 1px solid #e2e2e2;
+    box-shadow: 0px 5px 7px 0px #e2e2e2;
+`;
