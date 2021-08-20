ee62377f566d16f87aff9f79c77eeb1c769f5b50
Mabi Sousa <maria_gs_cruz@estudante.sc.senai.br>
2021-08-20 20:45:08 -0300

    Componentização dos filtros

---
+++ src/components/Filter/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Title, Form, Filters } from './style'
+import {IoIosArrowDown} from 'react-icons/io';
+import display1 from '../../imgs/display1.svg';
+import display2 from '../../imgs/display2.svg';
+
+const Filter: React.FC = () => (
+    <Filters>
+        <div>
+            <Title>PROJETOS ALOCADOS</Title>
+            <div>
+            <div id="filter">
+                <Form >
+                <label>Projeto:</label>
+                <input type="text" placeholder="Digite aqui... "/>
+                </Form>
+                <p>Seção:</p>
+                <div>
+                <span>Todos</span>
+                <IoIosArrowDown id="img" size={17}/>
+                </div>
+                <p>Status:</p>
+                <div>
+                <span>Todos</span>
+                <IoIosArrowDown id="img" size={17}/>
+                </div>
+            </div>
+            <a href="/projetos"> <img src={display1} /></a>
+            <a href="/display2"><img src={display2} /></a>
+            </div> 
+        </div>
+    </Filters>
+)
+
+export default Filter;
