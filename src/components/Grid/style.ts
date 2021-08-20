15d3c5c56e59e181ae05d88f00e37dcb6a9be0fb
Mabi Sousa <maria_gs_cruz@estudante.sc.senai.br>
2021-08-20 20:46:51 -0300

    Componentização do grid

---
+++ src/components/Grid/style.ts
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: row;
+    margin-left: 15px;
+    margin-top: 50px;
+
+    .box{
+        width: 50px;
+        height: 35px;
+        margin: 0 5px 5px 0;
+    }
+
+    .one{
+        background: #B4DDFB;
+    }
+
+    .two{
+        background: #90C0E3;
+    }
+
+    .three{
+        background: #448DCA;
+    }
+
+    .four{
+        background: #2E5CA6;
+    }
+
+    .five{
+        background: #023A67;
+    }
+  
+`;
