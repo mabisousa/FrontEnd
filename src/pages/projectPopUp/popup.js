7b2105e7c0222eef231c86c87096cff40a0b7b21
Mabi Sousa <maria_gs_cruz@estudante.sc.senai.br>
2021-08-20 21:01:43 -0300

    Criação da página Pop Up do projetos

---
+++ src/pages/projectPopUp/popup.js
@@ -0,0 +1,75 @@
+export function ContainerFunc(){
+    let cont = document.getElementById('container');
+    cont.style.width = '1175px';
+
+    let sideA = document.getElementById('sideA');
+    sideA.style.width = '35%';
+
+    let sideB = document.getElementById('sideB');
+    sideB.style.width = '29%';
+
+    let table = document.getElementById('table');
+    table.style.width = '310px';
+
+    let btn = document.getElementById('btn');
+    btn.style.width = '310px';
+
+    let sideC = document.getElementById('sideC');
+    sideC.style.display = 'block';
+    sideC.style.width = '36%';
+
+    let table2 = document.getElementById('table2');
+    table2.style.borderCollapse ='collapse';
+    table2.style.width= '100%';
+    table2.style.textAlign= 'center';
+
+    let tabela = document.getElementById('tabela');
+    tabela.style.width ='87.5%';
+    tabela.style.height = '45%';
+    tabela.style.marginRight = '15px';
+    tabela.style.marginBottom = '0';
+    tabela.style.marginTop = '105px';
+
+    let header1 = document.getElementById('header1.2');
+    header1.style.width ='200px';
+    header1.style.border = 'none';
+
+    let header2 = document.getElementById('header2.2');
+    header2.style.width ='500px';
+    header2.style.border = 'none';
+
+    let thead = document.getElementById('thead');
+    thead.style.width ='100%';
+    thead.style.background = '#0075B1';
+    thead.style.color = '#fff';
+    thead.style.height = '40px';
+    thead.style.display = 'inline-flex';
+    thead.style.textTransform = 'uppercase';
+    thead.style.fontSize = '14px';
+    thead.style.marginBottom = '15px';
+
+    let Ttr = document.getElementById('Ttr');
+    Ttr.style.marginTop ='5px';
+    Ttr.style.border ='none';
+    Ttr.style.color = '#fff';
+
+    let tbody = document.getElementById('tbody');
+    tbody.style.height ='280px';
+    tbody.style.overflowY = 'scroll';
+    tbody.style.overflowX = 'hidden';
+    tbody.style.display = 'block';
+    tbody.style.border = '1px solid #e2e2e280';
+    
+    let tr = document.getElementById('tr');
+    tr.style.margin ='15px';
+    tr.style.border ='1px solid #e2e2e280';
+    tr.style.color = 'rgba(0, 0, 0, 0.87)';
+    tr.style.height = '40px';
+    tr.style.fontSize = '14px';
+    tr.style.letterSpacing = '0.01071em';
+
+    let td = document.getElementById('td');
+    td.style.width ='500px';
+    td.style.borderRight= '1px solid #e2e2e280';
+}
+
