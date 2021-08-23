ff2b3cc54f7e361896a5d6281b122a064476d13d
jenasw <jeanrgl03@gmail.com>
2021-08-23 11:35:10 -0300

    Routes finalizada, permissões a serem aderidas as rotas com isPrivate

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
 import Login from '../pages/Login';
 import Aprovacao from '../pages/Aprovacao';
 import Consultores from '../pages/Consultores';
 import Projeto from '../pages/Home/index';
 import Projeto2 from '../pages/display';
 import popUp from '../pages/projectPopUp/index';
+import Home from '../pages/Home/index';
+import AprovacaoConsultores from '../pages/AprovacaoConsultor';
 
 const Routes: React.FC = () => (
   <Switch>
+    <Route path="/aprovacaoconsultores" exact component={AprovacaoConsultores}/>
     <Route path="/" exact component={Login}/>
+    <Route path="/home" exact component={Home}/>
     <Route path="/aprovacao" exact component={Aprovacao}/>
     <Route path="/consultores" component={Consultores}/>
     <Route path="/projetos" exact component={Projeto}/>
