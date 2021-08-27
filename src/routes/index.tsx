bb2b5e05de2ad53c21d8f2fee724f35cf4f9d7fb
mabisousa <maria_gs_cruz@estudante.sc.senai.br>
2021-08-27 11:41:21 -0300

    Alterações de rota

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,21 +6,17 @@ import Aprovacao from '../pages/Aprovacao';
 import Consultores from '../pages/Consultores';
 import Projeto from '../pages/Home/index';
 import Projeto2 from '../pages/display';
-import popUp from '../pages/projectPopUp/index';
 import Home from '../pages/Home/index';
-import AprovacaoConsultores from '../pages/AprovacaoConsultor';
 import PopUpAprovacoes from '../components/Request/index2';
 
 const Routes: React.FC = () => (
   <Switch>
-    <Route path="/aprovacaoconsultores" exact component={AprovacaoConsultores}/>
     <Route path="/" exact component={Login}/>
     <Route path="/home" exact component={Home}/>
     <Route path="/aprovacao" exact component={Aprovacao}/>
     <Route path="/consultores" component={Consultores}/>
     <Route path="/projetos" exact component={Projeto}/>
     <Route path="/display2" exact component={Projeto2}/>
-    <Route path="/projetos/popUp" exact component={popUp}/>
     <Route path="/popupAprovacao" exact component={PopUpAprovacoes}/>
   </Switch>
   );
