50a03fe39f8ca1e756591b93e7c1b879bde8beff
mabisousa <maria_gs_cruz@estudante.sc.senai.br>
2021-09-13 16:42:31 -0300

    Exclusão do arquivo JS com função openModal e closeModal

--- src/script/modal/script.js
+++
@@ -1,9 +0,0 @@
-export function openModal() {
-  var popup = document.getElementById("popup");
-  popup.style.visibility = "visible";
-}
-
-export function closeModal(){
-  var popup = document.getElementById("popup");
-  popup.style.visibility = "hidden";
-}
