f32ab8e19152f25af5a85e18484869c8976b26f0
mabisousa <maria_gs_cruz@estudante.sc.senai.br>
2021-09-13 11:41:18 -0300

    Alteração da função JS para TS

--- src/pages/Consultores/index.tsx
+++ src/pages/Consultores/index.tsx
@@ -7,7 +7,6 @@ import { HiUserCircle } from 'react-icons/hi';
 import { BsX } from 'react-icons/bs'
 import { Container, PopUpInfo, Content, Skills, HoldContent, PopUpTable } from "./style";
 import Grid from "../../components/Grid"
-import { openModal, closeModal } from '../../script/modal/script.js';
 import api from "../../services/api";
 
 interface Consultor{
@@ -18,6 +17,7 @@ interface Consultor{
 }
 
 const Login: React.FC = () => {
+  const [showPopup, setShowPopup] = useState(false);
   const [consultores, setConsultor] = useState<Consultor[]>([]);
 
   useEffect(() => {
@@ -66,105 +66,108 @@ const Login: React.FC = () => {
                 <td>{consultor.nome}</td>
                 <td>{consultor.status}</td> 
                 <td>2</td>
-                <button onClick={openModal} id="button"><td> + </td></button>
+                <button onClick={() => setShowPopup(!showPopup)} id="button"><td> + </td></button>
               </tr>
             ))}
           </tbody>
         </table>
         </Consultores>
       </main>
-      <Container id="popup">
-        <div id="hold">
-        <button onClick={closeModal}><BsX/></button>
-        <PopUpInfo>
-          <header>
-            <HiUserCircle/>
-            <div id="EmployeeInformation">
-                <h1>ISAC FREIRE BEZERRA - 67270</h1>
-                <p>Desenvolvedor WEB</p>
-                <p>E-mail: isac_bezerra@empresa.com</p>
-            </div>
-          </header>
-        </PopUpInfo>
-        <Content>
-          <Skills>
-            <h5>SKILLS</h5>
-            <HoldContent>
-                <p>UI / UX</p>
-                <p>Desing Responsivo</p>
-                <p>CSS e JavaScript Frameworks</p>
-                <p>ReactJS</p>
-                <p>Bootstrap</p>
-                <p>Debug</p>
-                <p>Git</p>
-                <p>Git</p>
-                <p>Git</p>
-                <p>Git</p>
-                <p>Git</p>
-            </HoldContent>
-          </Skills>
-          <PopUpTable>
-            <table>
-              <thead>
-                <tr>
-                  <td>NÚMERO</td>
-                  <td>STATUS</td>
-                  <td>PROJETO</td>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>0000000</td>
-                  <td>Concluido</td>
-                  <td>Restauração de alteradores</td> 
-                </tr>
-                <tr>
-                  <td>0000000</td>
-                  <td>Concluido</td>
-                  <td>Restauração de alteradores</td> 
-                </tr>
-                <tr>
-                  <td>0000000</td>
-                  <td>Concluido</td>
-                  <td>Restauração de alteradores</td> 
-                </tr>
-                <tr>
-                  <td>0000000</td>
-                  <td>Concluido</td>
-                  <td>Restauração de alteradores</td> 
-                </tr>
-                <tr>
-                  <td>0000000</td>
-                  <td>Concluido</td>
-                  <td>Restauração de alteradores</td> 
-                </tr>
-                <tr>
-                  <td>0000000</td>
-                  <td>Concluido</td>
-                  <td>Restauração de alteradores</td> 
-                </tr>
-                <tr>
-                  <td>0000000</td>
-                  <td>Concluido</td>
-                  <td>Restauração de alteradores</td> 
-                </tr>
-                <tr>
-                  <td>0000000</td>
-                  <td>Concluido</td>
-                  <td>Restauração de alteradores</td> 
-                </tr>
-                <tr>
-                  <td>0000000</td>
-                  <td>Concluido</td>
-                  <td>Restauração de alteradores</td> 
-                </tr>
-              </tbody>
-            </table>
-          </PopUpTable>
-        </Content>
-        <Grid/>
-        </div>
-      </Container>
+      {showPopup && 
+        <Container show={!!showPopup}>
+          <div id="hold">
+          <button onClick={() => setShowPopup(!showPopup)}><BsX/></button>
+          <PopUpInfo>
+            <header>
+              <HiUserCircle/>
+              <div id="EmployeeInformation">
+                  <h1>ISAC FREIRE BEZERRA - 67270</h1>
+                  <p>Desenvolvedor WEB</p>
+                  <p>E-mail: isac_bezerra@empresa.com</p>
+              </div>
+            </header>
+          </PopUpInfo>
+          <Content>
+            <Skills>
+              <h5>SKILLS</h5>
+              <HoldContent>
+                  <p>UI / UX</p>
+                  <p>Desing Responsivo</p>
+                  <p>CSS e JavaScript Frameworks</p>
+                  <p>ReactJS</p>
+                  <p>Bootstrap</p>
+                  <p>Debug</p>
+                  <p>Git</p>
+                  <p>Git</p>
+                  <p>Git</p>
+                  <p>Git</p>
+                  <p>Git</p>
+              </HoldContent>
+            </Skills>
+            <PopUpTable>
+              <table>
+                <thead>
+                  <tr>
+                    <td>NÚMERO</td>
+                    <td>STATUS</td>
+                    <td>PROJETO</td>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>0000000</td>
+                    <td>Concluido</td>
+                    <td>Restauração de alteradores</td> 
+                  </tr>
+                  <tr>
+                    <td>0000000</td>
+                    <td>Concluido</td>
+                    <td>Restauração de alteradores</td> 
+                  </tr>
+                  <tr>
+                    <td>0000000</td>
+                    <td>Concluido</td>
+                    <td>Restauração de alteradores</td> 
+                  </tr>
+                  <tr>
+                    <td>0000000</td>
+                    <td>Concluido</td>
+                    <td>Restauração de alteradores</td> 
+                  </tr>
+                  <tr>
+                    <td>0000000</td>
+                    <td>Concluido</td>
+                    <td>Restauração de alteradores</td> 
+                  </tr>
+                  <tr>
+                    <td>0000000</td>
+                    <td>Concluido</td>
+                    <td>Restauração de alteradores</td> 
+                  </tr>
+                  <tr>
+                    <td>0000000</td>
+                    <td>Concluido</td>
+                    <td>Restauração de alteradores</td> 
+                  </tr>
+                  <tr>
+                    <td>0000000</td>
+                    <td>Concluido</td>
+                    <td>Restauração de alteradores</td> 
+                  </tr>
+                  <tr>
+                    <td>0000000</td>
+                    <td>Concluido</td>
+                    <td>Restauração de alteradores</td> 
+                  </tr>
+                </tbody>
+              </table>
+            </PopUpTable>
+          </Content>
+          <Grid/>
+          </div>
+        </Container>
+      }
+      
     </>
 )};
 
