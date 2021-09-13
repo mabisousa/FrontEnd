aaedbfbb7e334e770a96430d354630937a42a052
Roberta Baseggio <roluiza2017@gmail.com>
2021-09-13 11:17:26 -0300

    Remoção da pagina de display em grade

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,6 @@ import Route from './Route';
 import Login from '../pages/Login';
 import Aprovacao from '../pages/Aprovacao';
 import Consultores from '../pages/Consultores';
-import Projeto2 from '../pages/display';
 import Home from '../pages/Home/index';
 
 const Routes: React.FC = () => (
@@ -14,7 +13,6 @@ const Routes: React.FC = () => (
     <Route path="/home" exact component={Home}/>
     <Route path="/aprovacao" exact component={Aprovacao}/>
     <Route path="/consultores" component={Consultores}/>
-    <Route path="/display2" exact component={Projeto2}/>
   </Switch>
   );
 
