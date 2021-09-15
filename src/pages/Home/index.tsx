7dc5ae29c8fabad4ea88673f8d3e307b8b979638
RobertaBaseggio <82897794+RobertaBaseggio@users.noreply.github.com>
2021-09-15 14:24:51 -0300

    Modificações na componentização

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { Cards, Card, TitleSection, TitleProject, HoldHours, Hours, Status, Date, Title, Form, Filters, Filter} from './style';
+import { Cards, Card, TitleSection, TitleProject, HoldHours, Hours, Status, Date, Title, Form, Filters, Filter, Container, TitlePopUp, InfosPopup, InfosGerais, Objetivo, Horas, HorasApontadas, Skills, Table, ConsultoresAlocados, DetailsPopup} from './style';
 import Profile from "../../components/Profile";
 import Header from "../../components/Header";
 import Menu from "../../components/Menu";
@@ -8,6 +8,9 @@ import display1 from '../../imgs/display1.svg';
 import display2 from '../../imgs/display2.svg';
 import api from "../../services/api"
 import Popup from "../../components/PopupProjetos"
+import Chart from 'react-google-charts';
+import { BsX } from 'react-icons/bs';
+import Grid from '../../components/Grid';
 
 interface Projetos{
   id: number,
@@ -28,10 +31,18 @@ interface Projetos{
 
 const Home: React.FC = () => {
 
-  const [projetos, setProjeto] = useState<Projetos[]>([]);
+  const [isOpen, setOpen] = useState(false);
   const [showPopup, setShowPopup] = useState(false);
-
+  const [projetos, setProjeto] = useState<Projetos[]>([]);
   const [show, setShow] = useState(false);
+
+  const handleOpen = useCallback(() => {
+      if(!!isOpen === false) {
+        setOpen(true);    
+      } else {
+        setOpen(false);    
+      }
+    }, [isOpen, setOpen]);
 
   const handleShow = useCallback(() => {
     if(!!show === false) {
@@ -85,7 +96,7 @@ const Home: React.FC = () => {
                 <button onClick={handleNotShow}><img src={display2} alt=""/></button>
             </div> 
         </div>
-    </Filters>
+      </Filters>
       <Cards > 
       {projetos.map((projeto) => (
         <Card Show={!!show} color="#EBB93A" onClick={() => setShowPopup(!showPopup)} key={projeto.id}>
@@ -113,10 +124,189 @@ const Home: React.FC = () => {
       ))}
       </Cards>
       {showPopup && 
-        <Popup/>
+        <Container Open={!!isOpen}  show={!!showPopup}>
+        <div id="hold">
+          <button onClick={() => setShowPopup(!showPopup)}><BsX/></button>
+          <TitlePopUp>
+            <h2> 0000 - SEÇÃO XYZ</h2>
+            <h1>0000000 - RESTAURAÇÃO DE ALTERADORES</h1>
+          </TitlePopUp>
+          <InfosPopup>
+            <InfosGerais Open={!!isOpen}  className="cont">
+              <h1>INFORMAÇÕES GERAIS:</h1>
+              <div>
+                <p>GESTOR RESPONSÁVEL: </p>
+                <p>FORNECEDOR: </p>
+              </div>
+            </InfosGerais>
+            <Objetivo Open={!!isOpen} className="cont">
+              <h1>OBJETIVO: </h1>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Consequuntur officiis ducimus ut debitis mollitia modi 
+                  tempora unde nobis reiciendis illum libero ipsam excepturi
+                  itaque aperiam quae assumenda praesentium, maxime consequatur.
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Consequuntur officiis ducimus ut debitis mollitia modi 
+                  tempora unde nobis reiciendis illum libero ipsam excepturi
+                  itaque aperiam quae assumenda praesentium, maxime consequatur.
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Consequuntur officiis ducimus ut debitis mollitia modi 
+                  tempora unde nobis reiciendis illum libero ipsam excepturi
+                  itaque aperiam quae assumenda praesentium, maxime consequatur.
+              </p>
+            </Objetivo>
+            <Horas Open={!!isOpen}  className="cont">
+              <h1>HORAS: </h1>
+              <HorasApontadas>
+                <Chart
+                  width={'160px'}
+                  height={'160px'}
+                  rootProps
+                  chartType="PieChart"
+                  data={[
+                    ['Task', 'Hours per Day'],  
+                    ['Total', 10],
+                    ['Apontadas', 10]
+                  ]}
+                  options={{
+                    pieHole: 0.7  ,
+                    tooltip: { trigger: 'none'},
+                    legend: 'none',
+                    backgroundColor: '',
+                    pieSliceText: 'none',
+                    slices: {
+                      0: { color: '57b7dc' },
+                      1: { color: 'B4DDFB' },
+                    },  
+                  }}
+                />
+                <div>
+                  <p>TOTAL:
+                    <span>1600h</span> 
+                  </p> 
+                  <p>APONTADAS:
+                    <span>800h</span>
+                  </p>
+                </div>
+              </HorasApontadas>
+            </Horas>
+            <Skills Open={!!isOpen}  className="cont">
+              <Table id="table">
+                <table>
+                  <thead>
+                    <tr>
+                      <td>SKILL</td>
+                      <td>HORAS APONTADAS</td>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>Desenvolvimento PHP</td>
+                      <td>40H</td>
+                    </tr>
+                    <tr>
+                      <td>Desenvolvimento JAVA</td>
+                      <td>15H</td>
+                    </tr>
+                    <tr>
+                      <td>Desenvolvimento React</td>
+                      <td>20H</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </Table>
+              <button id="visualizar" onClick={handleOpen}>VISUALIZAR CONSULTORES</button>
+            </Skills>
+            <ConsultoresAlocados Open={!!isOpen}>
+              <table>
+                <thead>
+                  <tr>
+                    <td>CADASTRO</td>
+                    <td>NOME</td>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>67270</td>
+                    <td >Isac Freire Bezerra</td>
+                  </tr>
+                  <tr>
+                    <td>67271</td>
+                    <td>Jean Henrique Reiguel</td>
+                  </tr>
+                  <tr>
+                    <td>67270</td>
+                    <td >Isac Freire Bezerra</td>
+                  </tr>
+                  <tr>
+                    <td>67271</td>
+                    <td>Jean Henrique Reiguel</td>
+                  </tr>
+                  <tr>
+                    <td>67270</td>
+                    <td >Isac Freire Bezerra</td>
+                  </tr>
+                  <tr>
+                    <td>67271</td>
+                    <td>Jean Henrique Reiguel</td>
+                  </tr>
+                  <tr>
+                    <td>67270</td>
+                    <td >Isac Freire Bezerra</td>
+                  </tr>
+                  <tr>
+                    <td>67271</td>
+                    <td>Jean Henrique Reiguel</td>
+                  </tr>
+                  <tr>
+                    <td>67271</td>
+                    <td>Jean Henrique Reiguel</td>
+                  </tr>
+                  <tr>
+                    <td>67270</td>
+                    <td >Isac Freire Bezerra</td>
+                  </tr>
+                  <tr>
+                    <td>67271</td>
+                    <td>Jean Henrique Reiguel</td>
+                  </tr><tr>
+                    <td>67271</td>
+                    <td>Jean Henrique Reiguel</td>
+                  </tr>
+                  <tr>
+                    <td>67270</td>
+                    <td >Isac Freire Bezerra</td>
+                  </tr>
+                  <tr>
+                    <td>67271</td>
+                    <td>Jean Henrique Reiguel</td>
+                  </tr><tr>
+                    <td>67271</td>
+                    <td>Jean Henrique Reiguel</td>
+                  </tr>
+                  <tr>
+                    <td>67270</td>
+                    <td >Isac Freire Bezerra</td>
+                  </tr>
+                  <tr>
+                    <td>67271</td>
+                    <td>Jean Henrique Reiguel</td>
+                  </tr>
+                </tbody>
+              </table>
+            </ConsultoresAlocados>
+          </InfosPopup>
+          <DetailsPopup>
+            <Grid/>
+            <h1>EM ANDAMENTO</h1>
+          </DetailsPopup>
+        </div>
+      </Container>
       }
     </>
-  )
+                
+  ) 
 };
 
 export default Home;
