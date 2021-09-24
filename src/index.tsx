1730086c2901d13527a01dd8f60838e213dce695
mabisousa <maria_gs_cruz@estudante.sc.senai.br>
2021-09-24 10:06:12 -0300

    Correção de alteração desnecessária

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,10 @@
-import React from "react";
-import { Container } from "./style";
-import { useTransition } from "react-spring";
-import Toast from "./Toast";
-import { messageContent } from "../../hooks/toast";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-interface ContainerProps{
-    message: messageContent[];
-}
-
-const ToastContainer: React.FC<ContainerProps> = ({ message }) => {
-    const messagesWithTransition = useTransition(
-        message,
-        (message) => message.id,
-        {
-            from: { right: "-120%" },
-            enter: { right: "0%" },
-            leave: { right: "-120%" }
-        }
-    )
-
-    return(
-        <Container>
-            {messagesWithTransition.map(({item, key, props}) =>(
-                <Toast key={key} style={props} message={item}/>
-            ))}
-        </Container>
-    )
-}
-
-export default ToastContainer;
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
