b95892e369bb68f34c92f801a842d35080cd6a19
mabisousa <maria_gs_cruz@estudante.sc.senai.br>
2021-10-18 11:36:58 -0300

    Darkmode notification

--- src/components/Notification/index.tsx
+++ src/components/Notification/index.tsx
@@ -1,13 +1,18 @@
-import React, { HTMLAttributes} from "react";
+import React, { HTMLAttributes, useState} from "react";
 
 import { Container } from './style'
 
 type NotificationProps = HTMLAttributes<HTMLDivElement>;
 
-const Notification: React.FC<NotificationProps> = (props) => (
-  <Container 
-    {...props}>
-  </Container>
-)
+const Notification: React.FC<NotificationProps> = (props) => {
+
+  const [darkMode] = useState(false);
+  
+  window.localStorage.getItem('');
+
+  return (
+    <Container darkMode={darkMode} {...props}/>
+  )
+}
 
 export default Notification;
--- src/components/Notification/style.ts
+++ src/components/Notification/style.ts
@@ -1,9 +1,22 @@
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 
-export const Container = styled.div`
+interface notificationProps{
+  darkMode: boolean;
+}
+
+export const Container = styled.div<notificationProps>`
+
+  ${(props) => props.darkMode ?
+    css`
+      background: #fff; 
+      box-shadow: 0px 5px 7px 0px #e2e2e2;
+    `: css`
+      background: #11181D;
+      box-shadow: 0px 5px 7px 0px rgba(0, 0, 0, 0.25);
+    `}
+
   min-height: 50px;
   max-width: 100%;
   margin:5px 0px;
-  border: 1px solid #e2e2e2;
-  box-shadow: 0px 5px 7px 0px #e2e2e2;
+ 
 `;
