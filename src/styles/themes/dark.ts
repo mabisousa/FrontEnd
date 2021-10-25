5f802b138624089cbada37d177c132ecdbaa7d88
mabisousa <maria_gs_cruz@estudante.sc.senai.br>
2021-10-25 11:45:12 -0300

    Arquivos de configurações para modo claro e escuro

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,11 @@
+export default {
+  title: 'dark',
+
+  colors: {
+    background: '#1F262B',
+    headerBackground: '#1B2126',
+    title: '#fff',
+    undeline: '#57B7DC',
+    iconColor: '#fff',
+  }
+}
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,11 @@
+export default {
+  title: 'light',
+
+  colors: {
+    background: '#fff',
+    headerBackground: '#fff',
+    title: '#000',
+    undeline: '#0075B1',
+    iconColor: '#7e7e7e'
+  }
+}
