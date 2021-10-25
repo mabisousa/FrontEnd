b088ce5153fc25e931e5eaebb187c5318a992c00
mabisousa <maria_gs_cruz@estudante.sc.senai.br>
2021-10-25 11:44:04 -0300

    modo escuro - instalações

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import light from './styles/themes/light';
+import dark from './styles/themes/dark';
+import Header from './components/Header';
 
 import GlobalStyle from './styles/global';
 import Routes from './routes';
 import AuthProvider from './hooks';
 
-const App: React.FC = () => (
-  <>
-    <BrowserRouter>
-      <AuthProvider>
-        <Routes />
-      </AuthProvider>
 
-      <GlobalStyle/>
-    </BrowserRouter>
+const App = () => {
+  const [theme, setTheme] = useState(light);
+
+  const toggleTheme = () => {
+    setTheme(theme.title === 'light' ? dark : light);
+  };
+  console.log(theme)
+  
+  return (
+  <>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <AuthProvider>
+          <Routes toggleTheme={toggleTheme}/>
+        </AuthProvider>
+        <Header toggleTheme={toggleTheme}/>
+
+        <GlobalStyle/>
+      </BrowserRouter>
+    </ThemeProvider>
   </>
-);
+  )
+};
 
 export default App;
