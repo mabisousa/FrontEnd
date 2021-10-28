613f1a88dbc036257a0ac6b501ab1aac918fff1b
mabisousa <maria_gs_cruz@estudante.sc.senai.br>
2021-10-28 11:41:44 -0300

    Arquivos de configurações para modo claro e escuro

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -10,6 +10,23 @@ declare module 'styled-components' {
       title: string;
       undeline: string;
       iconColor: string;
+      progresStatus: string;
+      concludedStatus: string;
+      lateStatus: string;
+      cardBackground: string;
+      titleSection: string;
+      date: string;
+      profileBorder: string;
+      profileBorderAfter: string;
+      projectMenu: string;
+      approvalMenu: string;
+      consultantMenu: string;
+      thead: string;
+      tableBorder: string;
+      inactiveStatus: string;
+      skillsButton: string;
+      pointedHours: string;
+      totalHours: string;
     }
   }
 }
