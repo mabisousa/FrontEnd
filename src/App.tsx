999e834bc4b94b82c40c7b0947969f1cc968f576
mabisousa <maria_gs_cruz@estudante.sc.senai.br>
2021-10-29 08:52:08 -0300

    função darkmode

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider, DefaultTheme } from 'styled-components';
 import light from './styles/themes/light';
 import dark from './styles/themes/dark';
 import Header from './components/Header';
+import usePersistedState from './utils/usePersistedState';
 
 import GlobalStyle from './styles/global';
 import Routes from './routes';
@@ -11,10 +11,11 @@ import AuthProvider from './hooks';
 
 
 const App = () => {
-  const [theme, setTheme] = useState(light);
+  const [theme, setTheme] = usePersistedState<DefaultTheme>('theme', light);
 
   const toggleTheme = () => {
     setTheme(theme.title === 'light' ? dark : light);
+    window.location = window.location
   };
   console.log(theme)
   
